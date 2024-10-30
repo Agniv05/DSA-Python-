0e292fbd9b948ee05dc85f84e33fd63608ea11f3
Agnivom <agnivmitra11@gmail.com>
2024-10-30 12:28:28 +0530

    Create array_push_pop_modify.py

---
+++ array_push_pop_modify.py
@@ -0,0 +1,37 @@
+# Initialize an array
+arr = [1, 2, 3]
+# Push (append) an element
+arr.append(4)
+print("After push:", arr)  # Output: [1, 2, 3, 4]
+
+# Pop the last element
+last_element = arr.pop()
+print("After pop:", arr)  # Output: [1, 2, 3]
+print("Popped element:", last_element)  # Output: 4
+# Pop an element at a specific index
+element_at_index_1 = arr.pop(1)
+print("After popping index 1:", arr)  # Output: [1, 3]
+print("Popped element at index 1:", element_at_index_1)  # Output: 2
+
+# Alter element at index 1
+arr[1] = 10
+print("After alteration:", arr)  # Output: [1, 10]
+
+
+
+# Initial array
+arrt = [1, 2, 3]
+# Push an element
+arrt.append(4)
+print("After push:", arrt)  # Output: [1, 2, 3, 4]
+
+# Pop the last element
+arrt.pop()
+print("After pop:", arrt)  # Output: [1, 2, 3]
+
+# Alter element at index 1
+arrt[1] = 10
+print("After alteration:", arr)  # Output: [1, 10, 3]
+
+
+
