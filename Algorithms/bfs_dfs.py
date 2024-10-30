196967459061e7e9715bb519aae60deefbc6439f
Agnivom <agnivmitra11@gmail.com>
2024-10-31 01:55:05 +0530

    Create bfs_dfs.py

---
+++ Algorithms/bfs_dfs.py
@@ -0,0 +1,20 @@
+from collections import deque
+
+def bfs(graph, start):
+    visited = set()
+    queue = deque([start])
+    while queue:
+        node = queue.popleft()
+        if node not in visited:
+            print(node, end=" ")
+            visited.add(node)
+            queue.extend(graph[node])
+
+def dfs(graph, start, visited=None):
+    if visited is None:
+        visited = set()
+    visited.add(start)
+    print(start, end=" ")
+    for neighbor in graph[start]:
+        if neighbor not in visited:
+            dfs(graph, neighbor, visited)
